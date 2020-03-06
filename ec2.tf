47ccfb150d9fcc37ddfd73b1a59e8535c7e3e2f0
Cloud User <centos@ip-172-31-24-96.eu-west-2.compute.internal>
2020-03-06 02:14:19 +0000

    imported an instance

--- ec2.tf
+++ ec2.tf
@@ -4,3 +4,9 @@ resource "aws_instance" "web" {
   instance_type = "t2.micro" 
   key_name = "${aws_key_pair.deployer.key_name}"
 } 
+
+resource "aws_instance" "web2" {
+    ami           = "ami-0a887e401f7654935"
+  instance_type = "t2.micro" 
+  key_name = "${aws_key_pair.deployer.key_name}"
+} 
