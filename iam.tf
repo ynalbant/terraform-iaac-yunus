04661b4bbd5a284b33f5509bc13d86c6b3d3e8ff
Cloud User <centos@ip-172-31-24-96.eu-west-2.compute.internal>
2020-03-07 20:51:56 +0000

    iam.tf edited

--- iam.tf
+++ iam.tf
@@ -27,8 +27,44 @@ resource "aws_iam_group_membership" "developers_team" {
   name = "developers-group-membership" 
   users = [ 
     "${aws_iam_user.tim.name}",
-    "${aws_iam_user.tben.name}",
+    "${aws_iam_user.ben.name}",
     "${aws_iam_user.lisa.name}",
   ] 
   group = "${aws_iam_group.developers.name}"
  }
+ 
+ #Policy
+  
+
+resource "aws_iam_policy" "policy" { 
+
+  name = "test_policy3" 
+
+  path = "/" 
+
+  description = "My test policy" 
+
+  policy = <<EOF 
+
+{ {
+    "Version": "2012-10-17",
+    "Statement": [
+        {
+            "Effect": "Allow",
+            "NotAction": [
+                "iam:*",
+                "organizations:*",
+                "account:*"
+            ],
+            "Resource": "*"
+        },
+        {
+            "Effect": "Allow",
+            "Action": [
+                "iam:CreateServiceLinkedRole",
+                "iam:DeleteServiceLinkedRole",
+                "iam:ListRoles",
+ 
+
+EOF 
+} 
