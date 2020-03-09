74d389fd335f6836d359fe14714b93aa8b36e65b
Cloud User <centos@ip-172-31-24-96.eu-west-2.compute.internal>
2020-03-09 23:50:28 +0000

    output.tf edited

--- output.tf
+++ output.tf
@@ -5,3 +5,19 @@ output "IP" {
 output "KEY" {
   value = "${module.wordpress.KEY}"
 }
+
+output "AMI" {
+  value = "${module.wordpress.AMI}"
+}
+
+output "SEC_GROUP" {
+  value = "${module.wordpress.SEC_GROUP}"
+}
+
+output "ROUTE53" {
+  value = "${module.wordpress.ROUTE53}"
+}
+
+output "bucketname" {
+  value = "${module.wordpress.bucketname}"
+}
