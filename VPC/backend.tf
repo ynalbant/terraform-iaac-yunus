daf56584bd40050c75c5d3a2158223ee49b64a4b
Cloud User <centos@ip-172-31-24-96.eu-west-2.compute.internal>
2020-03-11 01:14:27 +0000

    region edited

--- VPC/backend.tf
+++ VPC/backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "s3" {
     bucket = "terraform-class-yunus"
-    key    = "path/to/my/us-west-1"
+    key    = "path/to/my/us-east-1"
     region = "us-east-1"
     #dynamodb_table = "terraform-class"
   }
--- VPC/vpc.tfvars
+++ VPC/vpc.tfvars
@@ -1,5 +1,5 @@
 #provide a region for VPC
-region= "us-west-1"
+region= "us-east-1"
 
 # This is used for VPC CIDR BLOCK
 cidr_block= "10.0.0.0/16"
