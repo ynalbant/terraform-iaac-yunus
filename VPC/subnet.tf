8b3606d8ad354ac4c66ca9aacc7bdeb4eb0116cf
Cloud User <centos@ip-172-31-24-96.eu-west-2.compute.internal>
2020-03-11 00:36:11 +0000

    variables edited

--- VPC/subnet.tf
+++ VPC/subnet.tf
@@ -7,12 +7,12 @@ resource "aws_subnet" "dev1" {
 
 resource "aws_subnet" "dev2" {
   vpc_id = "${aws_vpc.dev.id}"
-  cidr_block = "10.0.2.0/24"
+  cidr_block = "${var.cidr_block2_public}"
 }
 
 resource "aws_subnet" "dev3" {
   vpc_id = "${aws_vpc.dev.id}"
-  cidr_block = "10.0.3.0/24"
+  cidr_block = "${var.cidr_block3_public}"
 }
 
 
@@ -20,15 +20,15 @@ resource "aws_subnet" "dev3" {
 
 resource "aws_subnet" "dev_private1" {
   vpc_id = "${aws_vpc.dev.id}"
-  cidr_block = "10.0.101.0/24"
+  cidr_block = "${var.cidr_block1_private}"
 }
 
 resource "aws_subnet" "dev_private2" {
   vpc_id = "${aws_vpc.dev.id}"
-  cidr_block = "10.0.102.0/24"
+  cidr_block = "${var.cidr_block2_private}"
 }
 
 resource "aws_subnet" "dev_private3" {
   vpc_id = "${aws_vpc.dev.id}"
-  cidr_block = "10.0.103.0/24"
+  cidr_block = "${var.cidr_block3_private}"
 }
