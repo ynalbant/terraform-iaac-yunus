34508095aa462524bd9ca7cd6c6619be91cde794
Cloud User <centos@ip-172-31-24-96.eu-west-2.compute.internal>
2020-03-12 23:28:01 +0000

    subnets added to outpu.tf for configuration file

--- VPC/output.tf
+++ VPC/output.tf
@@ -1,3 +1,34 @@
 output "VPC_ID" {
   value       = "${module.dev.VPC_ID}"
 }
+
+output "Subnet1" {
+  value       = "${module.dev1.id}"
+ 
+}
+
+output "Subnet2" {
+  value       = "${module.dev2.id}"
+ 
+}
+
+output "Subnet3" {
+  value       = "${module.dev3.id}"
+ 
+}
+
+output "Private Subnet1" {
+  value       = "${module.dev_private1.id}"
+ 
+}
+
+output "Private Subnet2" {
+  value       = "${module.dev_private2.id}"
+ 
+}
+
+output "Private Subnet3" {
+  value       = "${module.dev_private3.id}"
+ 
+}
+
