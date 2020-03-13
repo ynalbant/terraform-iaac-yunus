a3a16c859d3f7476053f2a941d59dfc87d3ebaba
Cloud User <centos@ip-172-31-24-96.eu-west-2.compute.internal>
2020-03-13 00:32:43 +0000

    ++

--- data_source/ec2.tf
+++ data_source/ec2.tf
@@ -2,6 +2,8 @@ provider "aws" {
   region = "us-west-2"
 }
 
+# gets ubuntu AMI
+
 data "aws_ami" "ubuntu" {
   most_recent = true
 
@@ -18,6 +20,50 @@ data "aws_ami" "ubuntu" {
   owners = ["099720109477"] # Canonical
 }
 
-output "AMI_ID" {
+
+
+
+
+
+
+output "UBUNTU_AMI_ID" {
   value       = "${data.aws_ami.ubuntu.id}"
+}
+
+
+
+
+# gets centos  AMI
+
+data "aws_ami" "centos" {
+  most_recent = true
+
+  filter {
+    name   = "name"
+    values = ["CentOS Linux 7 x86_64 HVM EBS *"]
+  }
+
+  filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
+  }
+
+  owners = ["679593333241"] # Canonical
+}
+
+
+
+
+
+output "CENTOS_AMI_ID" {
+  value       = "${data.aws_ami.centos.id}"
+}
+
+resource "aws_instance" "web" {
+  ami           = "${data.aws_ami.ubuntu.id}"
+  instance_type = "t2.micro"
+
+  tags = {
+    Name = "HelloWorld"
+  }
 }
