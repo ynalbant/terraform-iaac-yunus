01c50172cb0c3ac36cc4fb1da6c3c4d65c1065f3
Cloud User <centos@ip-172-31-24-96.eu-west-2.compute.internal>
2020-03-13 00:05:55 +0000

    data_source added

---
+++ data_source/ec2.tf
@@ -0,0 +1,23 @@
+provider "aws" {
+  region = "us-west-2"
+}
+
+data "aws_ami" "ubuntu" {
+  most_recent = true
+
+  filter {
+    name   = "name"
+    values = ["ubuntu/images/hvm-ssd/ubuntu-trusty-14.04-amd64-server-*"]
+  }
+
+  filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
+  }
+
+  owners = ["099720109477"] # Canonical
+}
+
+output "AMI_ID" {
+  value       = "${data.aws_ami.ubuntu.id}"
+}
