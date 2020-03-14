185575db660b319beddc1402d631abb54fbc813c
Cloud User <centos@ip-172-31-24-96.eu-west-2.compute.internal>
2020-03-14 21:21:38 +0000

    autoscaling added for app.tf

--- 3-tier-app/app.tf
+++ 3-tier-app/app.tf
@@ -1,7 +1,44 @@
-resource "aws_instance" "web" {
-  ami           = "${data.aws_ami.ubuntu.id}"
-  instance_type = "t2.micro"
-  subnet_id = "${data.terraform_remote_state.dev.Subnet1}"
+
+  module "wordpress" {
+  name = "wordpress"
+  source = "terraform-aws-modules/autoscaling/aws"
+  version = "2.12.0"
+
+  # Launch configuration
+  lc_name = "wordpress-lc"
+
+  image_id        = "${data.aws_ami.ubuntu.id}"
+  instance_type   = "t2.micro"
+  security_groups = ["${aws_security_group.allow_tls.id}"]
 
   
-  }
+ 
+
+  # Auto scaling group
+  asg_name                  = "wordpress-asg"
+  vpc_zone_identifier       = ["${data.terraform_remote_state.dev.Subnet1}", 
+                               "${data.terraform_remote_state.dev.Subnet2}", 
+                               "${data.terraform_remote_state.dev.Subnet3}",
+                              ]
+  health_check_type         = "EC2"
+  min_size                  = 3
+  max_size                  = 48
+  desired_capacity          = 3
+  wait_for_capacity_timeout = 0
+
+  tags = [
+    {
+      key                 = "Environment"
+      value               = "dev"
+      propagate_at_launch = true
+    },
+    {
+      key                 = "Project"
+      value               = "megasecret"
+      propagate_at_launch = true
+    },
+  ]
+
+
+ 
+}
