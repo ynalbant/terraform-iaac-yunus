314a1ca992cc3bcd9ec1f956344cc9ddd1c1ac81
Cloud User <centos@ip-172-31-24-96.eu-west-2.compute.internal>
2020-03-14 19:26:24 +0000

    different AZs added for subnets

--- VPC/module/subnet.tf
+++ VPC/module/subnet.tf
@@ -3,16 +3,19 @@
 resource "aws_subnet" "dev1" {
   vpc_id = "${aws_vpc.dev.id}"
   cidr_block = "${var.cidr_block1_public}"
+  availability_zone = "${var.public_subnet1}"
 }
 
 resource "aws_subnet" "dev2" {
   vpc_id = "${aws_vpc.dev.id}"
   cidr_block = "${var.cidr_block2_public}"
+  availability_zone = "${var.public_subnet2}"
 }
 
 resource "aws_subnet" "dev3" {
   vpc_id = "${aws_vpc.dev.id}"
   cidr_block = "${var.cidr_block3_public}"
+  availability_zone = "${var.public_subnet3}"
 }
 
 
